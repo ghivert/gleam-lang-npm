7d996c4b02b621140c118bd75fc76feab6c96bb5
Guillaume Hivert <hivert.is.coming@gmail.com>
2024-05-16 16:20:50 +0200

    first commit

---
+++ src/main.mjs
@@ -0,0 +1,66 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import * as tar from 'tar'
+
+function getArch() {
+  switch (process.arch) {
+    case 'arm64':
+      return 'aarch64'
+    case 'x64':
+      return 'x86_64'
+    default:
+      null
+  }
+}
+
+function getPlatform() {
+  switch (process.platform) {
+    case 'darwin':
+      return 'apple-darwin'
+    case 'linux':
+      return 'unknown-linux-musl'
+    case 'win32':
+      return 'pc-windows-msvc'
+    default:
+      return null
+  }
+}
+
+function generateEndpoint(version, arch, platform) {
+  return `https://github.com/gleam-lang/gleam/releases/download/${version}/gleam-${version}-${arch}-${platform}.tar.gz`
+}
+
+async function install() {
+  try {
+    const file = new URL(import.meta.url)
+    const dirname = path.dirname(file.pathname)
+    const { version, bin } = await fs.promises
+      .readFile(path.resolve(dirname, '../package.json'), 'utf-8')
+      .then(JSON.parse)
+      .then(package_ => ({ version: `v1.0.0`, bin: path.dirname(package_.bin.gleam) }))
+    // .then(package_ => ({ version: `v${package_.version}`, bin: path.dirname(package_.bin.gleam) }))
+    const binDir = path.resolve(dirname, '..', bin)
+    await fs.promises.mkdir(binDir, { recursive: true })
+    const arch = getArch()
+    const platform = getPlatform()
+    const endpoint = generateEndpoint(version, arch, platform)
+    const tgzPath = path.resolve(`/tmp/gleam-${version}-${arch}-${platform}.tgz`)
+    if (!fs.existsSync(tgzPath)) {
+      const tgz = await fetch(endpoint).then(res => res.arrayBuffer())
+      await fs.promises.writeFile(tgzPath, Buffer.from(tgz))
+    }
+    await tar.extract({ file: tgzPath, cwd: binDir })
+  } catch (error) {
+    console.log(error)
+    console.error(
+      [
+        '--- ERROR ----------------------------------------',
+        'It looks your computer does not support gleam yet.',
+        'Currently, gleam support macOS, Linux and Windows.',
+        '--------------------------------------------------',
+      ].join('\n')
+    )
+  }
+}
+
+install()
