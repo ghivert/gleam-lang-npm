31d4112fac93fbc45f95a5949359d3b53baed2e2
Guillaume Hivert <hivert.is.coming@gmail.com>
2024-05-16 17:41:47 +0200

    feat: add correct support for gleam
    Signed-off-by: Guillaume Hivert <hivert.is.coming@gmail.com>

--- src/main.mjs
+++ src/main.mjs
@@ -31,32 +31,62 @@ function generateEndpoint(version, arch, platform) {
   return `https://github.com/gleam-lang/gleam/releases/download/${version}/gleam-${version}-${arch}-${platform}.tar.gz`
 }
 
-async function install() {
+function getDirname() {
+  const file = new URL(import.meta.url)
+  const dirname = path.dirname(file.pathname)
+  return dirname
+}
+
+async function getVersion(dirname) {
+  return await fs.promises
+    .readFile(path.resolve(dirname, '../package.json'), 'utf-8')
+    .then(JSON.parse)
+    .then(package_ => `v${package_.version}`)
+}
+
+async function getInfos(dirname) {
+  const arch = getArch()
+  const platform = getPlatform()
+  const version = await getVersion(dirname)
+  return { arch, platform, version }
+}
+
+async function getArchivePath(dirname, cacheDir) {
+  const { arch, version, platform } = await getInfos(dirname)
+  const fileName = `gleam-${version}-${arch}-${platform}.tgz`
+  const tgzPath = path.resolve(cacheDir, fileName)
+  return { tgzPath, arch, version, platform }
+}
+
+async function dlCompilerArchive({ tgzPath, version, arch, platform }) {
+  const endpoint = generateEndpoint(version, arch, platform)
+  const tgz = await fetch(endpoint).then(res => {
+    if (!res.ok) throw new Error('Unreachable')
+    return res.arrayBuffer()
+  })
+  await fs.promises.writeFile(tgzPath, Buffer.from(tgz))
+}
+
+async function install(options) {
+  const dirname = getDirname()
+  const binDir = path.resolve(dirname, '..', 'bin')
+
+  const cacheDir = cachedir('gleam-npm')
+  await fs.promises.mkdir(cacheDir, { recursive: true })
+
   try {
-    const file = new URL(import.meta.url)
-    const dirname = path.dirname(file.pathname)
-    const { version, bin } = await fs.promises
-      .readFile(path.resolve(dirname, '../package.json'), 'utf-8')
-      .then(JSON.parse)
-      .then(package_ => ({
-        version: `v${package_.version}`,
-        bin: path.dirname(package_.bin.gleam),
-      }))
-    const binDir = path.resolve(dirname, '..', bin)
-    await fs.promises.mkdir(binDir, { recursive: true })
-    const arch = getArch()
-    const platform = getPlatform()
-    const endpoint = generateEndpoint(version, arch, platform)
-    const cacheDir = cachedir('gleam-npm')
-    await fs.promises.mkdir(cacheDir, { recursive: true })
-    const fileName = `gleam-${version}-${arch}-${platform}.tgz`
-    const tgzPath = path.resolve(cacheDir, fileName)
-    if (!fs.existsSync(tgzPath)) {
-      const tgz = await fetch(endpoint).then(res => res.arrayBuffer())
-      await fs.promises.writeFile(tgzPath, Buffer.from(tgz))
-    }
-    await tar.extract({ file: tgzPath, cwd: binDir })
+    const data = await getArchivePath(dirname, cacheDir)
+    if (!fs.existsSync(data.tgzPath)) await dlCompilerArchive(data)
+    await tar.extract({ file: data.tgzPath, cwd: binDir })
   } catch (error) {
+    const isBadArchive = error.message.includes('TAR_BAD_ARCHIVE')
+    if (isBadArchive && !options?.propagateErrors) {
+      const { tgzPath } = await getArchivePath(dirname, cacheDir)
+      if (fs.existsSync(tgzPath)) {
+        await fs.promises.rm(tgzPath)
+        return install({ propagateErrors: true })
+      }
+    }
     console.error(error)
     console.error(
       [
