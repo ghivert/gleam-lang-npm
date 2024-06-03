105c505c64abbc68b1ea2a397655f5aea3817b1c
Guillaume Hivert <hivert.is.coming@gmail.com>
2024-06-03 12:05:54 +0200

    feat: add ability to ignore scripts
    Signed-off-by: Guillaume Hivert <hivert.is.coming@gmail.com>

--- bin/cli.mjs
+++ bin/cli.mjs
@@ -1,8 +1,22 @@
 #!/usr/bin/env node
 import * as child_process from 'child_process'
+import * as fs from 'fs'
 import * as path from 'path'
 
+// Replaces __dirname.
 const file = new URL(import.meta.url)
 const dirname = path.dirname(file.pathname)
 const binaryPath = path.resolve(dirname, 'gleam')
-child_process.spawn(binaryPath, process.argv.slice(2), { stdio: 'inherit' }).on('exit', process.exit)
+
+// User can run install with ignore scripts, compiler should be downloaded before
+// using it.
+const isExec = fs.existsSync(binaryPath)
+if (!isExec) {
+  const module = await import('../src/installer.mjs')
+  await module.install()
+}
+
+// Run the compiler.
+child_process
+  .spawn(binaryPath, process.argv.slice(2), { stdio: 'inherit' })
+  .on('exit', process.exit)
---
+++ src/main.mjs
@@ -0,0 +1,3 @@
+import { install } from './installer.mjs'
+
+install()
